792f77e8b2f804f65e890e418446e169a2573d38
Rashedul Hassan <rashedul@outlook.com>
2023-09-23 21:22:05 +0100

    Remove deprecated Permissions package and replace with Device

--- notifications/init.js
+++ notifications/init.js
@@ -1,21 +1,25 @@
 import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
+import * as Device from "expo-device";
 
 export const initializeNotifications = async () => {
 	// Request notification permissions
 	const requestNotificationPermission = async () => {
-		const { status: existingStatus } = await Permissions.getAsync(
-			Permissions.NOTIFICATIONS
-		);
-
-		if (existingStatus !== "granted") {
-			const { status } = await Permissions.askAsync(
-				Permissions.NOTIFICATIONS
-			);
-			if (status !== "granted") {
-				alert("You must allow notifications to receive them.");
+		if (Device.isDevice) {
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== "granted") {
+				const { status } =
+					await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== "granted") {
+				alert("Failed to get push token for push notification!");
 				return;
 			}
+		} else {
+			console.log("Must use physical device for Push Notifications");
 		}
 	};
 
